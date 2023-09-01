56794ae4f3d66230ec234514d2b47f5908b687b4
Alunos <deneribeiro10@hotmail.com>
2023-09-01 10:29:18 -0300

    add tasks hook

--- tasks-project/src/App.jsx
+++ tasks-project/src/App.jsx
@@ -3,12 +3,33 @@ import {Container, Main} from './mainStyle'
 import './reset.css'
 import Header from './components/Header'
 
+import { v4 as uuidv4 } from "uuid"
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: uuidv4(),
+      isComplete: false,
+      title: "texto...",
+    },
+    {
+      id: uuidv4(),
+      isComplete: true,
+      title: "texto2...",
+    },
+  ]);
+
   return (
     <Container>
       <Header />
       <Main>
-        Hello world
+        {
+          tasks && 
+          tasks.length > 0 &&
+          tasks.map((task) => (
+            <div style={{color: "white"}}>{task.title}</div>
+          ))
+        }
       </Main>
     </Container>
   )
